d3847cea43aa46587ccf9746a7d014b51fb76f95
ngxtp <ngxtp@outlook.com>
2021-05-11 16:53:00 +0800

    Update ci.sh

--- ci/ci.sh
+++ ci/ci.sh
@@ -3,8 +3,8 @@ set -e
 set -o pipefail
 
 docker login -u="$DOCKER_PRO_REGISTRY_USER" -p="$DOCKER_PRO_REGISTRY_PASSWORD" docker.seadrive.org
-docker pull docker.seadrive.org/seafileltd/seafile-pro:latest
-docker tag docker.seadrive.org/seafileltd/seafile-pro ngxq/seafile-pro
+docker pull docker.seadrive.org/seafileltd/seafile-pro-mc:latest
+docker tag docker.seadrive.org/seafileltd/seafile-pro-mc:latest ngxq/seafile-pro-mc:latest
 # refer: https://docs.travis-ci.com/user/docker/#branch-based-registry-pushes
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push ngxq/seafile-pro
+docker push ngxq/seafile-pro-mc:latest
